44cb240524fb8993d511a7fcf972dd856ca79d96
Jim Fulton <jim@zope.com>
2003-05-20 20:29:21 +0000

    Reformatted some excessively long lines.

--- expressions.py
+++ expressions.py
@@ -13,7 +13,7 @@
 ##############################################################################
 """Basic Page Template expression types.
 
-$Id: expressions.py,v 1.3 2003/05/01 19:35:50 faassen Exp $
+$Id: expressions.py,v 1.4 2003/05/20 20:29:21 jim Exp $
 """
 __metaclass__ = type # All classes are new style when run with Python 2.2+
 
@@ -57,7 +57,8 @@ class SubPathExpr:
                     compiledpath.append(tuple(currentpath))
                     currentpath=[]
                 if not _valid_name(element[1:]):
-                    raise CompilerError, 'Invalid variable name "%s"' % element[1:]
+                    raise CompilerError('Invalid variable name "%s"'
+                                        % element[1:])
                 compiledpath.append(element[1:])
             else:
                 match = namespace_re.match(element)
@@ -67,13 +68,17 @@ class SubPathExpr:
                         currentpath=[]
                     namespace, functionname = match.groups()
                     if not _valid_name(namespace):
-                        raise CompilerError, 'Invalid namespace name "%s"' % namespace
+                        raise CompilerError('Invalid namespace name "%s"'
+                                            % namespace)
                     if not _valid_name(functionname):
-                        raise CompilerError, 'Invalid function name "%s"' % functionname
+                        raise CompilerError('Invalid function name "%s"'
+                                            % functionname)
                     try:
-                        compiledpath.append(self._engine.getFunctionNamespace(namespace))
+                        compiledpath.append(
+                            self._engine.getFunctionNamespace(namespace))
                     except KeyError:
-                        raise CompilerError, 'Unknown namespace "%s"' % namespace
+                        raise CompilerError('Unknown namespace "%s"'
+                                            % namespace)
                     currentpath.append(functionname)
                 else:
                     currentpath.append(element)
@@ -86,10 +91,12 @@ class SubPathExpr:
         
         if callable(first):
             # check for initial function
-            raise CompilerError,'Namespace function specified in first subpath element'
+            raise CompilerError(
+                'Namespace function specified in first subpath element')
         elif isinstance(first,StringTypes):
             # check for initial ?
-            raise CompilerError,'Dynamic name specified in first subpath element'
+            raise CompilerError(
+                'Dynamic name specified in first subpath element')
         
         if not _valid_name(base):
             raise CompilerError, 'Invalid variable name "%s"' % element
