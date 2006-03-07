90642659287a7c0d9af8a555c55831cb3455d1d2
Tres Seaver <tseaver@palladion.com>
2006-03-07 12:48:54 +0000

    Eggify

---
+++ setup.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.deprecation package
+
+$Id$
+"""
+
+import os
+
+
+try:
+    from setuptools import setup, Extension
+    
+except ImportError, e:
+    from distutils.core import setup, Extension
+
+setup(name='zope.deprecation',
+      version='3.0',
+
+      url='http://svn.zope.org/zope.deprecation',
+      license='ZPL 2.1',
+      description='Zope 3 Deprection Framework',
+      author='Zope Corporation and Contributors',
+      author_email='zope3-dev@zope.org',
+      long_description='',
+
+      packages=['zope', 'zope.deprecation'],
+      package_dir = {'': os.path.join(os.path.dirname(__file__), 'src')},
+
+      namespace_packages=['zope',],
+      tests_require = ['zope.testing'],
+      include_package_data = True,
+
+      zip_safe = False,
+      )
+    
