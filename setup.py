cc1aaceb146e30c952597c69bb27260f7997dd38
Alexander Heavner <alex@zope.com>
2007-02-14 21:32:27 +0000

    Changed the version number in setup.py. Used find_packages to find packages opposed to listing them out. Removed setup.cfg.in, INSTALL.txt, develop.py, CHANGES.txt, test.py, MANIFEST.in and README.txt because they are no longer needed. Changed the try statement in __init__.py

--- setup.py
+++ setup.py
@@ -18,15 +18,10 @@ $Id$
 
 import os
 
-
-try:
-    from setuptools import setup, Extension
-    
-except ImportError, e:
-    from distutils.core import setup, Extension
+from setuptools import setup, find_packages
 
 setup(name='zope.tales',
-      version='3.3-dev',
+      version='3.3dev',
 
       url='http://svn.zope.org/zope.tales',
       license='ZPL 2.1',
@@ -35,7 +30,7 @@ setup(name='zope.tales',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
 
-      packages=['zope', 'zope.tales'],
+      packages=find_packages('src'),
       package_dir = {'': 'src'},
 
       namespace_packages=['zope',],
