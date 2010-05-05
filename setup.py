65a2a2f1a842a27972c3ef1638f033f2fdf73be0
Tres Seaver <tseaver@palladion.com>
2010-05-05 19:31:33 +0000

    Conform to repository policy.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2007 Zope Corporation and Contributors.
+# Copyright (c) 2007 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -28,7 +28,7 @@ def read(*rnames):
 
 setup(name='zope.tales',
       version = '3.5.2dev',
-      author='Zope Corporation and Contributors',
+      author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Template Application Language Expression Syntax '
                   '(TALES)',
