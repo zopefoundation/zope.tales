6276483fef974510cd59b73c6851e1867821e0e8
Tres Seaver <tseaver@palladion.com>
2012-05-23 15:22:12 +0000

    Prep 3.4.1 release.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.tales',
-      version = '3.4.1dev',
+      version = '3.4.1',
       author='Zope Corporation and Contributors',
       author_email='zope3-dev@zope.org',
       description='Zope 3 Template Application Language Expression Syntax '
