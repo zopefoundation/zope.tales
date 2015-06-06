992f74fc1a1488cbfdb9dc115ebbef226088158e
Tres Seaver <tseaver@palladion.com>
2015-06-06 12:51:57 -0400

    svb

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def alltests():
     return unittest.TestSuite(suites)
 
 setup(name='zope.tales',
-      version='4.1.1',
+      version='4.1.2.dev0',
       author='Zope Foundation and Contributors',
       author_email='zope-dev@zope.org',
       description='Zope Template Application Language Expression Syntax '
