168b9a2680a4a1bdfe611e13853df5f309f96c58
Stephan Richter <stephan.richter@gmail.com>
2019-06-26 09:06:38 -0400

    Back to development: 5.0.2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='zope.tales',
-    version='5.0.1',
+    version='5.0.2.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Zope Template Application Language Expression Syntax '
